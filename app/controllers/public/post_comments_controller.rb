aa7e12971448bda47c0059936045f9ab48705cec
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-10-11 08:53:37 +0000

    [Add] コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,7 +1,25 @@
 class Public::PostCommentsController < ApplicationController
-  def new
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_member.post_comments.new(post_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
   end
 
   def edit
   end
+
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def post_comment_params
+     params.require(:post_comment).permit(:comment)
+  end
+
 end
