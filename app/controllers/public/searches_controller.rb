817eefb5d19bdb5ea60a359004a080d366bd7af6
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-11-27 01:45:19 +0000

    [Update] 検索したキーワードの表示

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,11 +5,19 @@ class Public::SearchesController < ApplicationController
     category_id = params[:category_id]
     @word = params[:word]
     @books = Book.looks(params[:word])
+    @searched_category = Category.find_by(id: category_id) if category_id.present?
+
     if category_id.present?
       category_ids = CategoryBook.where(category_id: category_id).pluck(:book_id)
       book_ids = @books.ids
       @books = Book.where(id: category_ids & book_ids) #カテゴリIDと検索結果の両方に存在する書籍IDを見つけそれらのIDを持つ書籍を取得
     end
+
+    if @word.present? && category_id.present?
+      category_books_ids = CategoryBook.where(category_id: category_id).pluck(:book_id)
+      @books = @books.where(id: category_books_ids)
+    end
+
     @books = @books.where(status: :published).order(params[:desc]).page(params[:page]).per(4) #公開中のものを新しい順に４つずつ
   end
 
